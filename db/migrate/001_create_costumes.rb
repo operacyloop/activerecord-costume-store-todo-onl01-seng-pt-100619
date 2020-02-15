990f8ea962c845d0404be4b9002b171459d1f541
Barry Ivan Silber <age.ef.ess@gmail.com>
2020-02-15 21:30:06 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,6 +1,6 @@
 class CreateCostumes < ActiveRecord::Migration[5.2]
   def change
-    create_table :create_costumes do |t|
+    create_table :costumes do |t|
       t.string :name
       t.float :price
       t.string :image_url
